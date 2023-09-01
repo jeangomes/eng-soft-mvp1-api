751f1b047085e641035bf6eab2c67227d5c075f6
Jean Gomes <jeangomes.ti@gmail.com>
2023-08-31 23:46:49 -0300

    Receive request data

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask
+from flask import Flask, make_response, request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World from Python!'
+def home():
+    html = """<!DOCTYPE html>
+    <html>
+        <body>
+        <h1>Home temp</h1>
+        <p> Documentação em produção!! </p>
+        </body>
+    </html>"""
+    return make_response(html), 200
+
+
+@app.route('/add_purchase', methods=['POST'])
+def add_purchase():
+    # lendo atributos recebidos via formulário
+    produto = {}
+    # print(request.form['code'])
+    produto["code"] = request.form.get("code")
+    produto["quantity"] = int(request.form.get("quantity"))
+    produto["price"] = float(request.form.get("price"))
+    produto["purchase_date"] = request.form.get("purchase_date")
+    produto["purchase_amount"] = produto["quantity"] * produto["price"]
+    # imprimindo no console
+    print("Salvar produto:\n", produto)
+
+    # criando JSON
+    resposta_json = jsonify(produto)
+    # criando resposta
+    response = make_response(resposta_json, 200,)
+    response.headers["Content-Type"] = "application/json"
+
+    # retornando resposta
+    return response
 
 
 if __name__ == '__main__':
